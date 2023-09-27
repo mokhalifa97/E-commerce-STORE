8f826d8fee1c9cedd745887244106771c2f0a219
mohamed ezzat <mohamad.5halifa1997@gmail.com>
2023-09-27 23:03:49 +0300

    make relaeted and new product dynamic

--- app/Http/Controllers/DetailsController.php
+++ app/Http/Controllers/DetailsController.php
@@ -10,6 +10,8 @@ class DetailsController extends Controller
     
     public function show($id){
         $product= Product::findOrFail($id);
-        return view('SubPages.details',['product' =>$product]);
+        $Rproduct= Product::where('category_id',$product->category_id)->inRandomOrder()->limit(4)->get();
+        $Nproduct= Product::latest()->take(4)->get();
+        return view('SubPages.details',['product' =>$product,'rproducts'=>$Rproduct,'nproducts'=>$Nproduct]);
     }
 }
